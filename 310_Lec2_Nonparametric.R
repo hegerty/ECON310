042187924462e3c57053d3b5d8288c8c2c18662d
Scott W Hegerty <77708153+hegerty@users.noreply.github.com>
2022-03-31 12:16:25 -0500

    Add files via upload

--- 310_Lec2_Nonparametric.R
+++ 310_Lec2_Nonparametric.R
@@ -5,12 +5,13 @@
 
 # Generate 250 observations of random data
 set.seed(1)
-x<-runif(250,5,10)
+n<-250
+x<-runif(n,5,10)
 x
 # y is a function of x
-y<-2.5*x+rnorm(250,1,2)
+y<-2.5*x+rnorm(n,1,2)
 
-# Plot and get Pearson and Spearman correlations
+# Plot and get Pearson and Spearman correlations; also Kendall's tau
 plot(x,y,pch=20)
 cor(x,y)
 cor(x,y,method="spearman")
@@ -29,27 +30,34 @@ cor(x,y,method="kendall")
 data<-cbind(x,y,1,1,1)
 head(data)
 
-# Random assignment
-for(i in 1:250){
+# Random assignment among 3 groups
+for(i in 1:n){
   data[i,3]<-sample(c(1,2,3),1)
 }
 head(data)
 
 
-# Are the means different?
+# Are the means of x different?
 plot(data[,3],data[,1],pch=20)
+mean(data[data[,3]==1,1])
+mean(data[data[,3]==2,1])
+mean(data[data[,3]==3,1])
+#not significant
 kruskal.test(x= data[,1],g = data[,3])
 
-# Now split off the bottom 25%
+# Now split off the bottom 25% into the second of two groups 
 qv<-as.numeric(quantile(data[,1],.25))
 data[which(data[,1]<qv),4]<-2
 
 plot(data[,4],data[,1])
+# They clearly differ
+mean(data[data[,4]==1,1])
+mean(data[data[,4]==2,1])
 kruskal.test(x= data[,1],g = data[,4])
 
 # Now I introduce some randomness
+# Apply 1 and 2s
 qv<-as.numeric(quantile(data[,1],.5))
-
 
 data[which(data[,1]<qv),5]<-2
 
@@ -64,5 +72,40 @@ plot(data[,c(5,1)],pch=20,col=c("green","red"))
 abline(h=mean(data[which(data[,5]==2),1]),col="red")
 abline(h=mean(data[which(data[,5]==1),1]),col="green")
 kruskal.test(x= data[,1],g = data[,5])
+# Different results with different random data
 
+##################################################
+# Here is the Spearman correlation formula by hand
+## Original x and y from above
+
+# They match!
+x1<-rank(x)
+x1
+# no ties here but check this out
+rank(c(100,4,89,4,71,15))
+
+y1<-rank(y)
+D<-x1-y1
+D
+1-6*sum(D^2)/(n*(n^2-1))
+cor(x,y,method="spearman")
+
+########################################
+# Compare KW by hand
+kruskal.test(x= data[,1],g = data[,3])
+
+# ranks and groups
+R1<-mean(x1[data[,3]==1])
+R2<-mean(x1[data[,3]==2])
+R3<-mean(x1[data[,3]==3])
+l1<-nrow(data[data[,3]==1,])
+l2<-nrow(data[data[,3]==2,])
+l3<-nrow(data[data[,3]==3,])
+N<-l1+l2+l3
+N
+k<-3
+
+H<-(12/(N*(N+1)))*(l1*R1^2+l2*R2^2+l3*R3^2)-3*(N+1)
+H
+2*dchisq(H,k-1)
 
