7cba27efad8c30d8a01aee0e1eba4d64702d11bb
Scott W Hegerty <77708153+hegerty@users.noreply.github.com>
2022-04-03 11:32:39 -0500

    Add files via upload

---
+++ 310_Lec3_LogisticReg.R
@@ -0,0 +1,68 @@
+###############################################
+# Chicago Block Groups: logistic Regression
+# In class example, ECON 310 Spring 2022
+###############################################
+#install.packages("rms") <- This package gives pseudo-R2
+library(rms)
+
+# Get data
+data<-read.csv("https://raw.githubusercontent.com/hegerty/ECON310/main/310_Lec3_Data.csv")
+data<-na.omit(data)
+head(data)
+dim(data)
+
+# examine binary variable
+summary(data$BANKCOUNTB)
+head(data$BANKCOUNTB,40)
+
+# compare against count data
+summary(data$BANKCOUNT) # you could use poisson, negative binomial
+tail(data$BANKCOUNT,40)
+
+
+# Get some statistics
+colnames(data)
+summary(data[,6:13])
+
+# Run some logistic regressions:
+# DV: whether there is at least one bank in a given block group
+# Try some different explanatory variables
+# Which are significant?
+
+attach(data)
+lr0<-glm(BANKCOUNTB~PERCVAC+PERC75K+PERC25K,family = "binomial")
+summary(lr0)
+# Use rms package for pseudo-R-squared
+lrm0<-lrm(BANKCOUNTB~PERCVAC+PERC75K+PERC25K,data=data)
+lrm0
+lrm0$stats[10]
+
+lr1<-glm(BANKCOUNTB~PERCVAC+PERC75K,family = "binomial")
+summary(lr1)
+lrm1<-lrm(BANKCOUNTB~PERCVAC+PERC75K,data=data)
+
+
+lr2<-glm(BANKCOUNTB~PERCVAC+PERC25K,family = "binomial")
+summary(lr2)
+lrm2<-lrm(BANKCOUNTB~PERCVAC+PERC25K,data=data)
+
+lr3<-glm(BANKCOUNTB~PERCVAC+PERCBLK,family = "binomial")
+summary(lr3)
+lrm3<-lrm(BANKCOUNTB~PERCVAC+PERCBLK,data=data)
+
+lr4<-glm(BANKCOUNTB~PERCVAC+PERCWHT,family = "binomial")
+summary(lr4)
+lrm4<-lrm(BANKCOUNTB~PERCVAC+PERCWHT,data=data)
+
+# Compare Pseudo-R2
+pr2<-cbind(lrm0$stats[10],lrm1$stats[10],lrm2$stats[10],lrm3$stats[10],lrm4$stats[10])
+colnames(pr2)<-0:4
+pr2
+lrm3 # best choice?
+
+# plot bank variable vs. vacancy rate
+plot(PERCVAC,BANKCOUNTB,pch=20)
+plot(PERCBLK,BANKCOUNTB,pch=20)
+
+
+
---
+++ 310_Lec4_PCA.R
@@ -0,0 +1,108 @@
+#########################################################
+# Exchange Market Pressure Indices
+# Principal Components Analysis
+# In class example, ECON 310 Spring 2022
+########################################################
+
+# Generated EMP series (PCA) vs. s.d.-weighted indices
+# Get data
+# Set dates and examine
+# extra data; also unlabeled
+# 1995q2 - 2020q4, eight countries
+# I set this just to click "Source"
+
+data<-read.csv("https://raw.githubusercontent.com/hegerty/ECON310/main/310_Lec4_Data.csv")
+head(data)
+dim(data)
+data<-data[1:103,]
+colnames(data)
+tail(data)
+
+##########################################
+## PRINCIPAL COMPONENTS ANALYSIS
+## Much of this is extra!
+## You need 1) pcev (eigenvalues), 
+##          2) loadings (factor loadings)
+##          3) pc1 (new series)
+## You do not need a loop, table, etc.
+##########################################
+
+# set graphical parameters
+par(mfrow=c(4,2))
+par(mar=2*c(1,1,1,1))
+
+
+# set values for loop
+# 1-8 are original SD-weighted series; recalculated here
+jlist<-seq(10,33,by=3) # 3 components j=10 #10, 13, etc.
+
+#make some empty tables
+cortab<-NULL
+evtab<-NULL
+loadingstab<-NULL
+
+# loop for 8 pairs
+for(i in 1:8){
+s1<-data[,jlist[i]:(jlist[i]+2)] # 3 components here
+# s1[,2]<--s1[,2] # to make 2nd component negative, but it doesn't help
+emp1<-s1[,1]/sd(s1[,1])-s1[,2]/sd(s1[,2])+s1[,3]/sd(s1[,3]) # SD as alternate
+colnames(s1)<-c("dlnE","delRES","delrdiff")
+
+# do pca; here make tables manually
+pca<-prcomp(s1,center = TRUE,scale. = TRUE) # s1 is the set of variables you want to use
+#Eigenvalues
+pcev<-sqrt(pca$sdev)
+#Loadings: (will be 1 or 2)
+pcl<-pca$rotation
+loadings<-pcl[,1] # factor loadings
+loadingstab<-cbind(loadingstab,loadings)
+pcev # show eigenvalues
+evtab<-rbind(evtab,pcev)
+pcl[,1:2] # First two principal components
+pc1<-pca$x[,1]
+pc1<-ts(pc1,start=c(1995,2),frequency = 4)
+pc2<-pca$x[,2]
+pc1 # this is the generated series
+
+# calculate and plot series
+cor(pc1,emp1)
+ts.plot(pc1,emp1,main=colnames(data)[i+1],col=c("blue","red"))
+legend("bottomright",legend=c("PCA","SD"),lty=c(1,1),col=c("blue","red"),cex=.75,bty="n")
+abline(h=0)
+abline(h=1.5*sd(pc1),col="red",lty=2) # "Crisis" threshold, PC1
+
+cortab<-rbind(cortab,round(cor(pc1,emp1),3))
+}
+par(mfrow=c(1,1)) # return to normal
+
+# Clean up tables
+rownames(cortab)<-colnames(data)[2:9]
+colnames(cortab)<-"Corr."
+colnames(loadingstab)<-rownames(cortab)
+rownames(evtab)<-rownames(cortab)
+colnames(evtab)<-c("PC1","PC2","PC3")
+
+# Print results
+print("Eigenvalues")
+print(round(t(evtab),3)) #Eigenvalues (I decided to transpose this)
+print("Factor Loadings")
+print(round(loadingstab,3)) #factor loadings
+print("Correlations: SD and PCA")
+print(cortab) # correlation table
+
+#################################################
+## Just the main part
+## CHANGE VARIABLE NAMES, DATES AS NEEDED
+#################################################
+# s1<-         # CHANGE YOUR SET OF VARIABLES
+colnames(s1)<-c("dlnE","delRES","delrdiff") # change dates as needed
+pca<-prcomp(s1,center = TRUE,scale. = TRUE) # s1 is the set of variables you want to use
+pcev<-sqrt(pca$sdev)
+pcl<-pca$rotation # this gets overwritten
+loadings<-pcl[,1]
+pc1<-pca$x[,1]
+pc1<-ts(pc1,start=c(1995,2),frequency = 4)  # TS only; change dates if needed
+pc1 # this is the generated series
+pcev
+loadings
+plot(pc1)  # works for time series
